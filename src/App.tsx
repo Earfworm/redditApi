4ce8361cea9109a48f8d54be919d0c6952c76eb9
earfworm <deshawnreid2021@gmail.com>
2023-02-07 18:57:49 -0500

    need to get the children within data object in JSon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import RedditAwwJson from "./models/RedditAwwJsonModel";
+import { getRedditAwwData } from "./services/RedditAwwApiService";
 
 function App() {
+  const [redditData, setRedditData] = useState<RedditAwwJson>();
+  useEffect(() => {
+    getRedditAwwData().then((res) => setRedditData(res));
+  });
+  console.log(redditData?.data);
   return <div className="App"></div>;
 }
 
--- src/services/RedditAwwApiService.ts
+++ src/services/RedditAwwApiService.ts
@@ -1,1 +1,9 @@
+import axios from "axios";
+import RedditAwwJson from "../models/RedditAwwJsonModel";
 
+export const getRedditAwwData = (): Promise<RedditAwwJson> => {
+  return axios
+    .get(`https://www.reddit.com/r/aww/.json`)
+    .then((res) => res.data)
+    .catch((error) => console.log(error));
+};
